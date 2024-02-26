79bcc59145b8b162d908264bdb38b0c87afe4016
whitecity01 <kh081104@gmail.com>
2024-02-26 16:03:52 +0900

    Initial commit

---
+++ src/test/java/lostItem/demo/LostItemApplicationTests.java
@@ -0,0 +1,13 @@
+package lostItem.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
