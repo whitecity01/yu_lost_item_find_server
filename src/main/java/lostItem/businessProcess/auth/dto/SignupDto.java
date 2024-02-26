bf7c7e475b310c52a43a15e0480dda758c2a5420
whitecity01 <kh081104@gmail.com>
2024-02-26 22:37:11 +0900

    feat: 회원가입 Dto 추가

---
+++ src/main/java/lostItem/businessProcess/auth/dto/SignupDto.java
@@ -0,0 +1,23 @@
+package lostItem.businessProcess.auth.dto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignupDto {
+
+    @NotBlank(message = "이름은 필수값입니다.")
+    private String name;
+
+    @NotBlank(message = "이메일은 필수값입니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 필수 값입니다.")
+    private String password;
+
+}
