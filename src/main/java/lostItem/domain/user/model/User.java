671378b53d495ccd27c62cfe409616f2ddb533c6
whitecity01 <kh081104@gmail.com>
2024-02-26 22:37:34 +0900

    feat: user 엔티티 추가

---
+++ src/main/java/lostItem/domain/user/model/User.java
@@ -0,0 +1,36 @@
+package lostItem.domain.user.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String emailId;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+
+    public User(String name,String email, String password) {
+        this.id = null; //jpa가 알아서 관리해주기 때문에 null
+        this.name = name;
+        this.emailId = email;
+        this.password = password;
+    }
+
+}
