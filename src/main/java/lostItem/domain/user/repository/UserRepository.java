0079bb60cbee83ef630d8356b3a4c4847dfb3a97
whitecity01 <kh081104@gmail.com>
2024-02-26 22:38:45 +0900

    feat: UserRepository 인터페이스 추가

---
+++ src/main/java/lostItem/domain/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package lostItem.domain.user.repository;
+
+import lostItem.domain.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
+    Optional<User> findByEmailId(String emailId);
+}
