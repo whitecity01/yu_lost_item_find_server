ec5ca84803861ca6efa7672ce6849e66fe17692c
whitecity01 <kh081104@gmail.com>
2024-02-26 15:59:07 +0900

    Initial commit

---
+++ src/main/java/lostItem/demo/LostItemApplication.java
@@ -0,0 +1,13 @@
+package lostItem.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostItemApplication.class, args);
+	}
+
+}
