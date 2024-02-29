1ed1703574d0b6361e963984c674447301836048
whitecity01 <kh081104@gmail.com>
2024-03-01 00:55:50 +0900

    feat: cors 설정 추가

---
+++ src/main/java/lostItem/config/spring/WebConfig.java
@@ -0,0 +1,16 @@
+package lostItem.config.spring;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/auth/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST");
+    }
+}
