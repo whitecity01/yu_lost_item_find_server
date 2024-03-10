95584c8d80b074ab758ab20171aaaf5a0f8f05d1
whitecity01 <kh081104@gmail.com>
2024-03-10 17:04:19 +0900

    feat: s3ServiceImpl 추가 및 s3 이미지 저장 로직 구현

---
+++ src/main/java/lostItem/domain/service/S3ServiceImpl.java
@@ -0,0 +1,52 @@
+package lostItem.domain.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lostItem.Exception.collections.s3.ImageUploadException;
+import lostItem.dto.S3UrlDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.UUID;
+
+import static ch.qos.logback.classic.spi.ThrowableProxyVO.build;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class S3ServiceImpl implements S3Service{
+    private final AmazonS3 amazonS3;
+    @Value("${cloud.aws.s3.bucketName}")
+    private String bucketName; //버킷 이름
+    private String changedImageName(String originName) { //이미지 이름 중복 방지를 위해 랜덤으로 생성
+        String random = UUID.randomUUID().toString();
+        return random+originName;
+    }
+
+    @Override
+    public void uploadImageToS3(MultipartFile image) { //이미지를 S3에 업로드하고 이미지의 url을 반환
+        String originName = image.getOriginalFilename(); //원본 이미지 이름
+        String ext = originName.substring(originName.lastIndexOf(".")); //확장자
+        String changedName = changedImageName(originName); //새로 생성된 이미지 이름
+        ObjectMetadata metadata = new ObjectMetadata(); //메타데이터
+        metadata.setContentType("image/"+ext);
+        try {
+            PutObjectResult putObjectResult = amazonS3.putObject(new PutObjectRequest(
+                    bucketName, changedName, image.getInputStream(), metadata
+            ).withCannedAcl(CannedAccessControlList.PublicRead));
+
+        } catch (IOException e) {
+            throw new ImageUploadException(); //커스텀 예외 던짐.
+        }
+
+
+    }
+}
