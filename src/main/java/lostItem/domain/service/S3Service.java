6417380c43a33616de0090ca3e5fc031ebbd6dfb
whitecity01 <kh081104@gmail.com>
2024-03-10 17:03:46 +0900

    feat: s3Service 인터페이스 추가

---
+++ src/main/java/lostItem/domain/service/S3Service.java
@@ -0,0 +1,8 @@
+package lostItem.domain.service;
+
+import lostItem.dto.S3UrlDto;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface S3Service {
+    public void uploadImageToS3(MultipartFile image);
+}
