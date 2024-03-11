aa134626790a315978642438bd5b09a357b415c3
whitecity01 <kh081104@gmail.com>
2024-03-11 18:51:51 +0900

    modify: 생성자 함수 -> builder로 변경

--- src/main/java/lostItem/domain/model/ReportItem.java
+++ src/main/java/lostItem/domain/model/ReportItem.java
@@ -1,13 +1,14 @@
 package lostItem.domain.model;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "reportItem")
 public class ReportItem {
 
@@ -22,8 +23,8 @@ public class ReportItem {
     @Column(nullable = false, length = 255)
     private String assignLocation; // 맡긴 위치
 
-    @Column(nullable = false, length = 255)
-    private String findDate; // date type 수정
+    @Column(nullable = false)
+    private LocalDateTime findDate;
 
     @Column(nullable = false, length = 255)
     private String latitude; // 위도
@@ -34,13 +35,5 @@ public class ReportItem {
     @Column(nullable = false, length = 255)
     private String itemCategory; // 아이템 카테고리
 
-    public ReportItem(String title,String assignLocation, String findDate, String latitude, String longitude, String itemCategory) {
-        this.id = null; //jpa가 알아서 관리해주기 때문에 null
-        this.title = title;
-        this.assignLocation = assignLocation;
-        this.findDate = findDate;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.itemCategory = itemCategory;
-    }
+
 }
