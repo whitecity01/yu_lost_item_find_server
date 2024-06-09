be753ad280d87ecafe8ddaede55d0526f5495850
whitecity01 <kh081104@gmail.com>
2024-06-10 01:04:55 +0900

    modify: 분실물 리스트 반환 함수 로직 수정

--- src/main/java/lostItem/domain/repository/ReportItemRepository.java
+++ src/main/java/lostItem/domain/repository/ReportItemRepository.java
@@ -3,8 +3,10 @@ package lostItem.domain.repository;
 import lostItem.domain.model.ReportItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReportItemRepository extends JpaRepository<ReportItem, Long> {
     Optional<ReportItem> findByTitle(String title); // 타이틀 검색
+    List<ReportItem> findTop10ByTitleContainingIgnoreCase(String title); // 검색어 포함된 타이틀 최대 10개
 }
